b685b389d3b15a0e60193fc179c56e253fad18ca
Abi <amariano.fw@gmail.com>
2024-05-23 11:30:00 +0800

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Abi M on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
