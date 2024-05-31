51a08ab17383d291b7cec3a578bade0d319d10c7
Abi <amariano.fw@gmail.com>
2024-05-31 10:37:09 +0800

    Load mock data address in Map

---
+++ AirbnbClone/Extensions/Regions.swift
@@ -0,0 +1,16 @@
+//
+//  Regions.swift
+//  AirbnbClone
+//
+//  Created by Abi M on 5/29/24.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    static var lupao = CLLocationCoordinate2D(latitude: 15.8477, longitude: 120.9188)
+    static var miami = CLLocationCoordinate2D(latitude: 25.7617, longitude: 90.1918)
+    static var tokyo = CLLocationCoordinate2D(latitude: 35.6764, longitude: 139.6500)
+    static var losAngeles = CLLocationCoordinate2D(latitude: 34.0549, longitude: 118.2426)
+}
